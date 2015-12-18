2f8508e9e4970ee3fa1ff0fdbf2237273819a2e4
Laila Zouaki <laila_zouaki@gmail.com>
2015-12-18 09:41:43 +0100

    Mise en place du template

--- tdlog/tdlog/settings.py
+++ tdlog/tdlog/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'page_accueil', 
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'page_accueil/templates'),
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'tdlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
